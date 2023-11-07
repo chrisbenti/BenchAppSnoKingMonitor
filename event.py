2cbf567b82625e555bfda8b0fd6d6902d8b226d7
Chris Bentivenga <chris@chrisbenti.com>
2023-11-06 18:03:33 -0800

    tigers blood is weird

--- event.py
+++ event.py
@@ -15,12 +15,16 @@ class Event:
     @classmethod
     def from_ics(cls, ics):
         teams = ics.name.split(' vs. ')
-        return Event(ics.begin.datetime.timestamp(), ics.location.split(' - ')[0].replace('(', ''), teams[1], teams[0])
+        return Event(ics.begin.datetime.timestamp(), ics.location.split(' - ')[0].replace('(', ''), Event.normalize_team_name(teams[1]), Event.normalize_team_name(teams[0]))
 
     @classmethod
     def from_sno_king_site(cls, event):
-        return Event(parser.parse(event['dateTime']).timestamp(), event['rinkName'], event['teamHomeName'], event['teamAwayName'])
+        return Event(parser.parse(event['dateTime']).timestamp(), event['rinkName'], Event.normalize_team_name(event['teamHomeName']), Event.normalize_team_name(event['teamAwayName']))
 
     def __repr__(self) -> str:
         return f"{datetime.fromtimestamp(self.start)}: {self.homeTeam} vs {self.awayTeam} @ {self.location}"
+
+    @classmethod
+    def normalize_team_name(cls, team):
+        return team.replace("Shaved Ice: Tiger's Blood", "Tiger's Blood")
 
